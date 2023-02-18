7e1c56cbe02b3af20648fbe73508312eb510367c
Mauricio Santos <mauwebjc@gmail.com>
2023-02-17 21:20:16 -0300

    [Modulu02] Ajuste para Interface externa, ajuste de links e inclusão de subscribe

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,23 +1,33 @@
 import { LivroService } from './../../services/livro.service';
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent {
+export class ListaLivrosComponent implements OnDestroy {
 
   public listaLivros: [];
   public campoBusca: string = '';
+  private subscription: Subscription;
 
   constructor(private livroService: LivroService) { }
 
   public buscarLivros(): void {
-    console.log(`CampoBusca: ${this.campoBusca}`);
-    
-    this.livroService.buscar(this.campoBusca).subscribe(
-      retornoApi => console.log(retornoApi)
+    this.subscription = this.livroService.buscar(this.campoBusca).subscribe(
+      {
+        next: retornoApi => console.log(retornoApi),
+        error: erro => console.error(erro),
+        complete: () => console.log('Observable completado'),
+      }
     )
+  }
+
+  public ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+    console.log('ngOnDestroy');
+    
   }
 }
