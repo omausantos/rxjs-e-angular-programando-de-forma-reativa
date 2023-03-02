32eded83f3ce29f0d3b47e9d56b1214d579a1a0b
Mauricio Santos <mauwebjc@gmail.com>
2023-03-01 21:43:22 -0300

    [Modulo05] Uso de switchMap para controle de Fluxo para chamadas continuas

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,37 +1,28 @@
 import { LivroInterface } from "./../../interfaces/lista-livros.interface";
 import { LivroService } from "./../../services/livro.service";
 import { Component, OnDestroy } from "@angular/core";
-import { Subscription } from "rxjs";
+import { map, Subscription, switchMap, tap } from "rxjs";
 import { LivroModel } from "src/app/interfaces/livro.model";
+import { FormControl } from "@angular/forms";
 
 @Component({
   selector: "app-lista-livros",
   templateUrl: "./lista-livros.component.html",
   styleUrls: ["./lista-livros.component.css"],
 })
-export class ListaLivrosComponent implements OnDestroy {
-  public listaLivros: LivroModel[] = [];
-  public campoBusca: string = "";
-  private subscription: Subscription;
+export class ListaLivrosComponent {
+  public campoBusca = new FormControl();
 
   constructor(private livroService: LivroService) {}
 
-  public buscarLivros(): void {
-    this.subscription = this.livroService.buscar(this.campoBusca).subscribe({
-      next: (retornoApi) => this.converterDadosEmLivro(retornoApi),
-      error: (erro) => console.error(erro),
-      complete: () => console.log("Observable completado"),
-    });
-  }
+  public livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
+    switchMap((valorDigitado) => this.livroService.buscar(valorDigitado)),
+    map((listaItens) => this.converterDadosEmLivro(listaItens)),
+  );
 
-  private converterDadosEmLivro(lista: LivroInterface[]): void {
-    lista.map((item) => {
-      this.listaLivros.push(new LivroModel(item));
+  private converterDadosEmLivro(lista: LivroInterface[]): LivroModel[] {
+    return lista.map((item) => {
+      return new LivroModel(item);
     });
-  }
-
-  public ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-    console.log("ngOnDestroy");
   }
 }
