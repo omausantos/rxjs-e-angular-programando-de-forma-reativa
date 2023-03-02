a906b9a1c7f5e5b5d4887d3912927c63a879374f
Mauricio Santos <mauwebjc@gmail.com>
2023-03-01 22:00:04 -0300

    [Modulo05] Uso do debounceTime

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,21 +1,24 @@
 import { LivroInterface } from "./../../interfaces/lista-livros.interface";
 import { LivroService } from "./../../services/livro.service";
 import { Component } from "@angular/core";
-import { filter, map, switchMap } from "rxjs";
+import { debounceTime, filter, map, switchMap } from "rxjs";
 import { LivroModel } from "src/app/interfaces/livro.model";
 import { FormControl } from "@angular/forms";
 
+const TEMPOENTRECHAMADAS: number = 300;
 @Component({
   selector: "app-lista-livros",
   templateUrl: "./lista-livros.component.html",
   styleUrls: ["./lista-livros.component.css"],
 })
 export class ListaLivrosComponent {
+  
   public campoBusca = new FormControl();
 
   constructor(private livroService: LivroService) {}
 
   public livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
+    debounceTime(TEMPOENTRECHAMADAS),
     filter((valorDigitado) => this.minimoTextoParaPesquisa(valorDigitado)),
     switchMap((valorDigitado) => this.livroService.buscar(valorDigitado)),
     map((listaItens) => this.converterDadosEmLivro(listaItens))
